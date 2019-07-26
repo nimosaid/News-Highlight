923e49c169f5be415dd4283b37d8c30f66e61c3c
nimosaid <saidnimo123@gmail.com>
2019-07-26 11:35:45 +0300

    add changes

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,18 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # Initializing flask extentions
+    bootstrap.init_app(app)
+
+# Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+# Setting config
+    from.request import configure_request
+    configure_request(app)
+
+    # Will add views and forms
+
+    return app
