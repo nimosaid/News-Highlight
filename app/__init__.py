04a63354b91c3025d1e478ebd014aa3330287a74
nimosaid <saidnimo123@gmail.com>
2019-07-26 11:30:55 +0300

    add changes

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
